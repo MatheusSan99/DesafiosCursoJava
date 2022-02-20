e1b15a53757c6ca41988c6fff5eb2d31cb3a96bb
MatheusSan99 <99matheussan@gmail.com>
2022-02-20 13:18:00 -0300

    Término dos exercicios de fundamentos, entrando agora nas estruturas de controle.

---
+++ src/ExercicioDio.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExercicioDio {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        double A = scan.nextDouble();
+
+        double B = scan.nextDouble();
+
+        double valorAtual = ((B - A) * 100) / A;
+
+        System.out.println(df.format(valorAtual) + "%");
+
+
+    }
+}
---
+++ src/Teste.java
@@ -0,0 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String a = "";
+
+
+
+        while (!a.equalsIgnoreCase("Sair")) {
+            System.out.println();
+            a = scan.nextLine();
+
+
+        }
+       }
+    }
